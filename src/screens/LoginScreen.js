723e0607695ce56d393015eedc13380929e764e8
Sagar Goyal <sagargoyal2015@gmail.com>
2021-07-28 22:19:32 +0530

    Final Commit

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,82 +1,132 @@
-import React, {useState} from 'react'
-import { View, Text, Image, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Alert } from 'react-native'
-import {TextInput, Button} from 'react-native-paper';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Alert,
+  Linking,
+} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 import firebase from 'firebase';
 
-export default function LoginScreen({navigation}) {
+const GitHubRepo = 'https://github.com/thesagargoyal/react-photo-share';
 
-    const [email, setEmail]=useState('');
-    const [password, setPassword]=useState('');
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const userLogin = ()=>{
+  const openGitHub = async () => {
+    await Linking.openURL(GitHubRepo);
+  };
 
-        if(!email || !password)Alert.alert("Please enter required fields!");
-
-        else{
-            firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((result)=>{
-                console.log("signin");
-            })
-            .catch((error)=>{
-                Alert.alert("The password is invalid or the user does not have a password.");
-            })
-        }
+  const userLogin = () => {
+    if (!email || !password) Alert.alert('Please enter required fields!');
+    else {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((result) => {
+          console.log('signin');
+        })
+        .catch((error) => {
+          Alert.alert(
+            'The password is invalid or the user does not have a password.'
+          );
+        });
     }
+  };
 
-    return (
-        <KeyboardAvoidingView style={styles.main}>
-            <View style={styles.box1}>
-                <Image style={{width:200,height:200}} source={require('../assets/logo.png')}/>
-                
-            </View>
-            
-            <View style={styles.box2}>
-            <Text style={styles.text}>Please Login to continue</Text>
-                <TextInput mode="outlined" label="Email" value={email} onChangeText={text => setEmail(text)}/>
-                <TextInput mode="outlined" label="Password" value={password} onChangeText={text => setPassword(text)} secureTextEntry={true}/>
-                <Button  mode="contained" onPress={() => userLogin()}> Log In </Button>
-                <TouchableOpacity onPress={()=>navigation.navigate("signup")}><Text style={{textAlign: "center", fontWeight: "bold"}}>Don't have a account ? Sign Up !</Text></TouchableOpacity>
-            </View>
-            <View style={styles.author}>
-                <Image source={require('../../assets/github.png')} style={styles.github} />
-                <Text style={styles.githubText}>github.com/thesagargoyal</Text>
-            </View>
-        </KeyboardAvoidingView>
-    )
+  return (
+    <KeyboardAvoidingView style={styles.main}>
+      <View style={styles.box1}>
+        <Image
+          style={{ width: 200, height: 200 }}
+          source={require('../assets/logo.png')}
+        />
+      </View>
+
+      <View style={styles.box2}>
+        <Text style={styles.text}>Please Login to continue</Text>
+        <TextInput
+          mode="outlined"
+          label="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          mode="outlined"
+          label="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry={true}
+        />
+        <Button mode="contained" onPress={() => userLogin()}>
+          {' '}
+          Log In{' '}
+        </Button>
+        <TouchableOpacity onPress={() => navigation.navigate('signup')}>
+          <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
+            Don't have a account ? Sign Up !
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.author}>
+        <TouchableOpacity
+          onPress={() => {
+            openGitHub();
+          }}
+          style={styles.githubButton}>
+          <Image
+            source={require('../../assets/github.png')}
+            style={styles.github}
+          />
+          <Text style={styles.githubText}>GitHub</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    main:{
-        display:"flex",
-        justifyContent:"center",
-        flex:1
-    },
-    box1:{
-        alignItems:"center",
-        flex:3
-    },
-    box2:{
-        paddingHorizontal:40,
-        justifyContent:"space-evenly",
-        flex:5
-    },
-    text:{
-        fontSize:22,
-        textAlign: "center",
-    },
-    author: {
-        flex:2,
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    github: {
-        height: 40,
-        width: 40,
-    },
-    githubText: {
-        fontWeight: "bold",
-        fontSize:15
-    }
-})
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  box1: {
+    alignItems: 'center',
+    flex: 3,
+  },
+  box2: {
+    paddingHorizontal: 40,
+    justifyContent: 'space-evenly',
+    flex: 5,
+  },
+  text: {
+    fontSize: 22,
+    textAlign: 'center',
+  },
+  author: {
+    flex: 2,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  githubButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  github: {
+    height: 40,
+    width: 40,
+  },
+  githubText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -7,21 +7,28 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
   Alert,
-} from "react-native";
-import { TextInput, Button } from "react-native-paper";
+  Linking,
+} from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 // import auth from '../config'
-import firebase from "firebase";
-require("firebase/firestore");
-require("firebase/firebase-storage");
+import firebase from 'firebase';
+require('firebase/firestore');
+require('firebase/firebase-storage');
+
+const GitHubRepo = 'https://github.com/thesagargoyal/react-photo-share';
 
 export default function LoginScreen({ navigation }) {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const openGitHub = async () => {
+    await Linking.openURL(GitHubRepo);
+  };
 
   const userSignup = async () => {
     if (!email || !password || !name)
-      Alert.alert("Please enter required fields!");
+      Alert.alert('Please enter required fields!');
     else {
       console.log(name);
 
@@ -31,18 +38,18 @@ export default function LoginScreen({ navigation }) {
         .then(async (result) => {
           await firebase
             .firestore()
-            .collection("user")
+            .collection('user')
             .add({
               name,
               uid: firebase.auth().currentUser.uid,
             })
             .catch((err) => {
               console.log(err);
-              Alert.alert("Name not updated");
+              Alert.alert('Name not updated');
             });
         })
         .catch((error) => {
-          Alert.alert("Something went wrong!");
+          Alert.alert('Something went wrong!');
         });
     }
   };
@@ -52,12 +59,11 @@ export default function LoginScreen({ navigation }) {
       <View style={styles.box1}>
         <Image
           style={{ width: 200, height: 200 }}
-          source={require("../assets/logo.png")}
+          source={require('../assets/logo.png')}
         />
-        
       </View>
       <View style={styles.box2}>
-      <Text style={styles.text}>Please Sign Up to continue</Text>
+        <Text style={styles.text}>Please Sign Up to continue</Text>
         <TextInput
           mode="outlined"
           label="Name"
@@ -80,18 +86,24 @@ export default function LoginScreen({ navigation }) {
         <Button mode="contained" onPress={() => userSignup()}>
           Sign Up
         </Button>
-        <TouchableOpacity onPress={() => navigation.goBack("login")}>
-          <Text style={{ textAlign: "center", fontWeight: "bold"}}>
+        <TouchableOpacity onPress={() => navigation.goBack('login')}>
+          <Text style={{ textAlign: 'center', fontWeight: 'bold' }}>
             Already have a account ? Log In !
           </Text>
         </TouchableOpacity>
       </View>
       <View style={styles.author}>
-        <Image
-          source={require("../../assets/github.png")}
-          style={styles.github}
-        />
-        <Text style={styles.githubText}>github.com/thesagargoyal</Text>
+        <TouchableOpacity
+          onPress={() => {
+            openGitHub();
+          }}
+          style={styles.githubButton}>
+          <Image
+            source={require('../../assets/github.png')}
+            style={styles.github}
+          />
+          <Text style={styles.githubText}>GitHub</Text>
+        </TouchableOpacity>
       </View>
     </KeyboardAvoidingView>
   );
@@ -99,37 +111,40 @@ export default function LoginScreen({ navigation }) {
 
 const styles = StyleSheet.create({
   main: {
-    display: "flex",
-    justifyContent: "center",
+    display: 'flex',
+    justifyContent: 'center',
     flex: 1,
   },
   box1: {
-    alignItems: "center",
+    alignItems: 'center',
     flex: 3,
   },
   box2: {
     paddingHorizontal: 40,
-    height: "50%",
-    justifyContent: "space-evenly",
+    height: '50%',
+    justifyContent: 'space-evenly',
     flex: 5,
   },
   text: {
     fontSize: 22,
-    textAlign: "center",
+    textAlign: 'center',
   },
   author: {
     flex: 2,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  githubButton: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   github: {
     height: 40,
     width: 40,
   },
   githubText: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     fontSize: 15,
   },
 });
